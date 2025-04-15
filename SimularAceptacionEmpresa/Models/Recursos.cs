2e520ca25d2b972248265f9a0776cc9fa287434b
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2025-04-15 00:15:43 -0400

    Cree el modelo de Recursos

---
+++ SimularAceptacionEmpresa/Models/Recursos.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SimularAceptacionEmpresa.Models;
+public class Recursos
+{
+    [Key]
+    public int RecursoId { get; set; }
+    public string Nombre { get; set; }
+}
