d74a640affff32bd09843fce86c88eb799094952
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2025-04-15 00:15:26 -0400

    Cree el modelo de Preguntas

---
+++ SimularAceptacionEmpresa/Models/Preguntas.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SimularAceptacionEmpresa.Models;
+public class Preguntas
+{
+    [Key]
+    public int PreguntaId { get; set; }
+    public string Texto { get; set; } // Ej: ¿Qué tan satisfecho estás con el servicio?
+
+    [ForeignKey("PreguntaId")]
+    public ICollection<PreguntasDetalle> PreguntasDetalle { get; set; } = new List<PreguntasDetalle>();
+}
