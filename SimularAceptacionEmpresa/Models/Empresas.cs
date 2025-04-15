0c0368961e0d917878e51567b41eeabeaa829444
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2025-04-15 00:14:42 -0400

    Cree el modelo de empresas

---
+++ SimularAceptacionEmpresa/Models/Empresas.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SimularAceptacionEmpresa.Models;
+public class Empresas
+{
+    [Key]
+    public int EmpresaId { get; set; }
+    public string Nombre { get; set; }
+}
