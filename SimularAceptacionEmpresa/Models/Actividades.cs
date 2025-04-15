df4a539ef032310924de7365b10fb02aebb9d53d
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2025-04-15 00:14:25 -0400

    Cree el modelo de Actividades

---
+++ SimularAceptacionEmpresa/Models/Actividades.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SimularAceptacionEmpresa.Models;
+
+public class Actividades
+{
+    [Key]
+    public string ActividadId { get; set; }
+    public string Nombre { get; set; }
+    public int valoracion { get; set; } // Ej: 1-10
+    [ForeignKey("Empresas")]
+    public int EmpresaId { get; set; } // ID de la empresa a la que pertenece la actividad
+
+}
