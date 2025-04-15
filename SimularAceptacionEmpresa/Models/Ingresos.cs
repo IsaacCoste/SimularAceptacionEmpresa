654f7de60635a7a2d85b246f0eeabda21e2309a6
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2025-04-15 00:15:04 -0400

    Cree el modelo de Ingresos

---
+++ SimularAceptacionEmpresa/Models/Ingresos.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SimularAceptacionEmpresa.Models;
+public class Ingresos
+{
+    [Key]
+    public int IngresoId { get; set; }
+    public string Tipo { get; set; } // Ej: Ventas, Despacho, Publicidad
+}
